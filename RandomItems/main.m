ba1657e2499388d32d567856e6cfec6b9bd68433
Blurredbits <mmorris00@mac.com>
2014-02-25 10:10:42 -0700

    Initial commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Mark J Morris on 2/25/14.
+//  Copyright (c) 2014 Mark J Morris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
